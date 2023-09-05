f8b3e68b8334c34803be8e46760a45071cb5c508
Utkarshvr <utkarshv995@gmail.com>
2023-09-05 17:14:25 +0530

    Chart Done

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -25,27 +25,39 @@ const RevenueChart = ({ chartData }) => {
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="month" />
+
+        {/* Left Y-Axis for Bars */}
         <YAxis
+          yAxisId="left"
           tickFormatter={(value) => (value / 10000000).toFixed(2) + " Cr"}
         />
+
+        {/* Right Y-Axis for Lines */}
+        <YAxis
+          yAxisId="right"
+          orientation="right" // This places the axis on the right side
+          tickFormatter={(value) => (value / 10000000).toFixed(2) + " Cr"}
+        />
+
         <Tooltip formatter={(value) => (value / 10000000).toFixed(2) + " Cr"} />
         <Legend />
-        {/* {chartData?.data?.map((department, index) => {
+        {chartData?.data?.map((department, index) => {
           return (
             <Bar
               key={index}
               dataKey={department?.department}
               stackId="a"
               fill={getColorForDepartment(index)}
+              yAxisId="left" // Associate this Bar series with the left Y-Axis
             />
           );
-        })} */}
-        <Bar type="linear" dataKey="total" fill={getColorForDepartment(0)} />
+        })}
         <Line
           type="linear"
           dataKey="cumulativeTotal"
           stroke="#f39c12"
           strokeWidth={4}
+          yAxisId="right" // Associate this Line series with the right Y-Axis
         />
       </ComposedChart>
     </ResponsiveContainer>
