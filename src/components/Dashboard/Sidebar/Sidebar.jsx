edb93aff8c848ad3d9d475f84a32890a8b034ba0
Utkarshvr <utkarshv995@gmail.com>
2023-09-22 17:11:33 +0530

    side bar fxd

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -11,40 +11,40 @@ const Sidebar = () => {
         <div className="dlabnav-scroll">
           <ul className="metismenu" id="menu">
             {/* Sales Dashboard */}
-            {auth?.dept?.some((r) => r.role.name === "SALES") ? (
-              <li>
-                <a
-                  className="has-arrow ai-icon"
-                  href="javascript:void()"
-                  aria-expanded="false"
-                >
-                  <LuLayoutDashboard className="fs-2" />
-                  <span className="nav-text fs-4"> Dashboard</span>
-                </a>
-                <ul aria-expanded="false">
-                  <li>
+            <li>
+              <a
+                className="has-arrow ai-icon"
+                href="javascript:void()"
+                aria-expanded="false"
+              >
+                <LuLayoutDashboard className="fs-2" />
+                <span className="nav-text fs-4"> Dashboard</span>
+              </a>
+              <ul aria-expanded="false">
+                <li>
+                  {auth?.dept?.some((r) => r.role.name === "SALES") && (
                     <NavLink to="/dashboard" className="fs-4">
                       Sales Dashboard
                     </NavLink>
-                    {auth?.dept?.some((r) => r.role.name === "WAREHOUSE") && (
-                      <NavLink to="/dashboard/warehouse" className="fs-4">
-                        Warehousing
-                      </NavLink>
-                    )}
-                    {auth?.dept?.some((r) => r.role.name === "FINANCE") && (
-                      <NavLink to="/dashboard/accounting" className="fs-4">
-                        Accounting
-                      </NavLink>
-                    )}
-                    {auth?.dept?.some((r) => r.role.name === "ENGINEERING") && (
-                      <NavLink to="/dashboard/eng" className="fs-4">
-                        Engineering
-                      </NavLink>
-                    )}
-                  </li>
-                </ul>
-              </li>
-            ) : null}
+                  )}
+                  {auth?.dept?.some((r) => r.role.name === "WAREHOUSE") && (
+                    <NavLink to="/dashboard/warehouse" className="fs-4">
+                      Warehousing
+                    </NavLink>
+                  )}
+                  {auth?.dept?.some((r) => r.role.name === "FINANCE") && (
+                    <NavLink to="/dashboard/accounting" className="fs-4">
+                      Accounting
+                    </NavLink>
+                  )}
+                  {auth?.dept?.some((r) => r.role.name === "ENGINEERING") && (
+                    <NavLink to="/dashboard/eng" className="fs-4">
+                      Engineering
+                    </NavLink>
+                  )}
+                </li>
+              </ul>
+            </li>
             {/* sales */}
             {auth?.dept?.some((r) => r.role.name === "SALES") ? (
               <li>
