3d81fcd7734239c5d4589ef868990c68bf477b06
Utkarshvr <utkarshv995@gmail.com>
2023-09-25 15:07:59 +0530

    GRN & Stock Setup

--- src/components/Warehouse/WarehouseGRN.jsx
+++ src/components/Warehouse/WarehouseGRN.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import { useState } from "react";
+import Loader from "../../ui/Loader";
+import DataTable from "react-data-table-component";
 
 export default function WarehouseGRN() {
+  const [loading, setLoading] = useState(false);
+
+  // columns for table
+  const columns = [
+    {
+      name: "GRN",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+    {
+      name: "PO",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+    {
+      name: "Vendor",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+    {
+      name: "Qty Expected",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+    {
+      name: "Qty Received",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+    {
+      name: "Status",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+  ];
   return (
-    <div>WarehouseGRN</div>
-  )
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <h1 className="text-center">GRN</h1>
+
+          <div className="card">
+            <div className="card-body">
+              <DataTable
+                data={[{ name: "Hello" }]}
+                columns={columns}
+                customStyles={{
+                  rows: {
+                    style: {
+                      fontSize: "16px",
+                    },
+                  },
+                  headCells: {
+                    style: {
+                      fontSize: "19px",
+                      width: "170px",
+                    },
+                  },
+                }}
+                noContextMenu
+                fixedHeader
+                fixedHeaderScrollHeight="550px"
+                pagination
+                striped
+                highlightOnHover
+                subHeader
+                // actions={
+                //   <>
+                //     <h3 className="bg-primary text-white rounded-0 p-3">
+                //       Total: {numDifferentiation(total)}
+                //     </h3>
+                // </>
+                // }
+              />
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
--- src/components/Warehouse/WarehouseStock.jsx
+++ src/components/Warehouse/WarehouseStock.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import { useState } from "react";
+import Loader from "../../ui/Loader";
+import DataTable from "react-data-table-component";
 
 export default function WarehouseStock() {
+  const [loading, setLoading] = useState(false);
+
+  // columns for table
+  const columns = [
+    {
+      name: "Part No",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+    {
+      name: "Desc",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+    {
+      name: "Qty",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+    {
+      name: "Value",
+      selector: (row) => row?.name,
+      sortable: true,
+    },
+  ];
   return (
-    <div>WarehouseStock</div>
-  )
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <h1 className="text-center">Stock List</h1>
+
+          <div className="card">
+            <div className="card-body">
+              <DataTable
+                data={[{ name: "Hello" }]}
+                columns={columns}
+                customStyles={{
+                  rows: {
+                    style: {
+                      fontSize: "16px",
+                    },
+                  },
+                  headCells: {
+                    style: {
+                      fontSize: "19px",
+                      width: "170px",
+                    },
+                  },
+                }}
+                noContextMenu
+                fixedHeader
+                fixedHeaderScrollHeight="550px"
+                pagination
+                striped
+                highlightOnHover
+                subHeader
+                // actions={
+                //   <>
+                //     <h3 className="bg-primary text-white rounded-0 p-3">
+                //       Total: {numDifferentiation(total)}
+                //     </h3>
+                // </>
+                // }
+              />
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
