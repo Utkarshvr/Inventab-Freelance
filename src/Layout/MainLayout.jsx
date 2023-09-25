340c876bf901569b221f8fa7523e9886c4d1c0b8
Utkarshvr <utkarshv995@gmail.com>
2023-09-25 12:30:04 +0530

    Fix Route glitch

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,13 +1,15 @@
 import { Toaster } from "react-hot-toast";
-import { Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const MainLayout = () => {
   const location = useLocation();
 
   const noHeaderFooter = location.pathname.includes("/login");
+  const isRootPath = location.pathname === "/";
 
   return (
     <div>
+      {isRootPath ? <Navigate to="/dashboard" /> : null}
       {noHeaderFooter || ""}
       <Toaster />
       <Outlet />
