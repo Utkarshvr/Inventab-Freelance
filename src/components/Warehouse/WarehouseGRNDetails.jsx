19a9f0163092c73922911ee5fd1ea1d51000dc27
Utkarshvr <utkarshv995@gmail.com>
2023-09-25 17:31:12 +0530

    GRN Setup

--- src/components/Warehouse/WarehouseGRNDetails.jsx
+++ src/components/Warehouse/WarehouseGRNDetails.jsx
@@ -8,11 +8,48 @@ import SectionTitle from "../Shared/SectionTitle";
 export default function WarehouseGRNDetails() {
   const [loading, setLoading] = useState(false);
 
+  const [serialNumbers, setSerialNumbers] = useState([]);
+
   // columns for table
   const columns = [
     {
       name: "Part No",
-      selector: (row) => row?.name,
+      selector: (row) => (
+        <p
+          onClick={() => {
+            // fetchSerialNumbers(index);
+            setSerializedNo(part.id);
+          }}
+          className="text-primary link_txt"
+          data-bs-toggle="modal"
+          data-bs-target="#serializedNumsModal"
+          style={{ cursor: "pointer" }}
+        >
+          {row?.name}
+        </p>
+        // {part?.parts_no?.serialization ? (
+        //   <>
+        //     {" "}
+        //     <td
+        //       onClick={() => {
+        //         fetchSerialNumbers(index);
+        //         setSerializedNo(part.id);
+        //       }}
+        //       className="text-primary link_txt"
+        //       data-bs-toggle="modal"
+        //       data-bs-target="#exampleModal"
+        //       style={{ cursor: "pointer" }}
+        //     >
+        //       {/* {`${part?.short_description} (abcdefgh)`} */}
+        //       {`${part?.short_description} (${part?.parts_no?.customer_part_number}) `}
+        //     </td>
+        //   </>
+        // ) : (
+        //   <td className="text-black">
+        //     {part?.short_description}
+        //   </td>
+        // )}
+      ),
       sortable: true,
     },
     {
@@ -82,6 +119,51 @@ export default function WarehouseGRNDetails() {
                 // </>
                 // }
               />
+            </div>
+          </div>
+
+          {/* Modal for serial no */}
+          <div
+            className="modal fade"
+            id="serializedNumsModal"
+            aria-labelledby="serializedNumsModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="table-responsive">
+                  <table className="table table-bordered text-center">
+                    <thead style={{ background: "#343A40" }}>
+                      <tr>
+                        <th className="text-light ps-4 fs-5">No</th>
+                        <th className="text-light ps-4 fs-5">Serial No</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          {serialNumbers?.map((sn, index) => {
+                            return <p key={sn}>{"Sl-" + ++index}</p>;
+                          })}
+                        </td>
+                        <td>
+                          {serialNumbers?.map((sn) => {
+                            return <p key={sn}>{sn}</p>;
+                          })}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
         </>
